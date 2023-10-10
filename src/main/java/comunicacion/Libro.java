e79e7e84f405778e036927e27d04e5efa02d8170
ronalcastro97 <105658996+ronalcastro97@users.noreply.github.com>
2023-10-10 00:13:42 -0500

    Update Libro.java

--- src/main/java/comunicacion/Libro.java
+++ src/main/java/comunicacion/Libro.java
@@ -53,7 +53,13 @@ public class Libro extends Escrito{
 		return this.interpretacion;
 	}
 	public String toString() {
-		return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+ getCo_autor()+"\n"+  getEditorial()+"\n" + getEdicion();
+		return this.getOrigen() + "\n"+
+				this.getTitulo()+ "\n"+
+				this.getAutor() +"\n" +
+				this.getPaginas() + "\n"+
+				this.getCo_autor() + "\n"+
+				this.getEditorial() + "\n"+
+				this.getEdicion();	
 	}
 	
 	

528d57d28c0c7648a3d245e2a4402813b4fabeda
ronalcastro97 <105658996+ronalcastro97@users.noreply.github.com>
2023-10-09 21:32:44 -0500

    taller 7- java

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,21 @@
+package comunicacion;
+
+public  class Alfabeto extends Pictograma{
+	private String[] letras;
+	private String interpretacion;
+	public Alfabeto(String origen, String[] letras,String interpretacion) {
+		super(origen);
+		this.letras=letras;
+		this.interpretacion=interpretacion;
+	}
+	public String interpretacion() {
+		return null;
+	}
+	public String toString() {
+		return null;
+	}
+	
+	}
+	
+	
+
